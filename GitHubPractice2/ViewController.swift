f05b3c5e1c47bbed04661a1666c5b7286881d16d
Miguel Aenlle <bot.developer3@gmail.com>
2021-12-10 10:19:51 -0600

    added code

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -7,8 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let animalThings = ["dog", "cat", "bird"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
